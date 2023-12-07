02c1e308a9c7c40f17842c2338a4fcaa974072af
Badmajor <vtk88@mail.ru>
2023-12-07 17:45:56 +0300

    done 3.0

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -2,24 +2,18 @@ from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 
-from .views import PostViewSet, GroupViewSet, CommentView
+from .views import PostViewSet, GroupViewSet, CommentViewSet
 
 router = DefaultRouter()
+subrouter = DefaultRouter()
 
 router.register(r'posts', PostViewSet)
 router.register(r'groups', GroupViewSet)
+subrouter.register(r'comments', CommentViewSet)
 
 
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),
-    path('', include(router.urls)),
-    path('posts/<int:post_id>/comments/<int:pk>/',
-         CommentView.as_view(
-             {
-                 'get': 'retrieve',
-                 'delete': 'destroy',
-                 'put': 'update',
-                 'patch': 'update'
-             },
-         )
-         )]
+    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/', include(router.urls)),
+    path('v1/posts/<int:post_id>/', include(subrouter.urls)),
+]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,56 +1,45 @@
 from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, \
-    DestroyModelMixin
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
 
 from posts.models import Comment, Group, Post
-from .permissions import CustomPermission
+from .permissions import AuthorchangeOrUserReadOnly
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [CustomPermission]
+    permission_classes = [AuthorchangeOrUserReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-    @action(detail=True, methods=['get', 'post', 'delete'])
-    def comments(self, request, *args, **kwargs):
-        post = self.get_object()
-        if request.method == 'GET':
-            data = Comment.objects.filter(post=post)
-            serializer = CommentSerializer(data, many=True)
-            return Response(serializer.data)
-        if request.method == 'POST':
-            serializer = CommentSerializer(data=request.data)
-            if serializer.is_valid():
-                serializer.save(post=post, author=request.user)
-                return Response(
-                    serializer.data,
-                    status=status.HTTP_201_CREATED
-                )
-            return Response(
-                data=serializer.errors,
-                status=status.HTTP_400_BAD_REQUEST
-            )
 
-
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [IsAuthenticated]
-
-    def create(self, request, *args, **kwargs):
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
 
-class CommentView(GenericViewSet, RetrieveModelMixin,
-                  UpdateModelMixin, DestroyModelMixin):
+class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [CustomPermission]
+    permission_classes = [AuthorchangeOrUserReadOnly]
+
+    def list(self, request, *args, **kwargs):
+        queryset = Comment.objects.filter(post=kwargs.get('post_id'))
+        serializer = CommentSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        post = Post.objects.get(pk=kwargs.get('post_id'))
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(post=post, author=request.user)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data=serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
